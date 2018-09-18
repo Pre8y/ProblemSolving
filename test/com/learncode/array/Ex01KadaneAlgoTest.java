5639946d60e8907b294181f729cbcc3ad4298135
Pre8y <er.preeti.patwa@gmail.com>
2018-09-18 18:13:16 +0530

    fixed

--- test/com/learncode/array/Ex01KadaneAlgoTest.java
+++ test/com/learncode/array/Ex01KadaneAlgoTest.java
@@ -6,7 +6,7 @@ class Ex01KadaneAlgoTest {
 
     @org.junit.jupiter.api.Test
     void findMaxSum() {
-        int[] a = {1, 2, 3};
-        assertEquals(6, new Ex01KadaneAlgo().findMaxSum(a, 3));
+        int[] a = {-2, 5, -1};
+        assertEquals(5, new Ex01KadaneAlgo().findMaxSum(a, 3));
     }
 }
